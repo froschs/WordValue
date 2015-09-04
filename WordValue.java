00b9fa6f0cc549083ac68673024fe78182ba75a6
froschs <froschs@my.wccc.edu>
2015-09-04 18:25:37 -0400

    WordValue Commit
    Commiting a change to WordValue project.

---
+++ WordValue.java
@@ -0,0 +1,44 @@
+
+package wordvalue;
+
+/**
+ *
+ * @author Seth Frosch
+ */
+public class WordValue {
+
+
+    public static void main(String[] args) {
+    char char1 = 'B';
+    char char2 = 'e';
+    char char3 = 'a';
+    char char4 = 'u';
+    char char5 = 't';
+    char char6 = 'i';
+    char char7 = 'f';
+    char char8 = 'u';
+    char char9 = 'l';
+    int int1 = (int)char1;
+    int int2 = (int)char2;
+    int int3 = (int)char3;
+    int int4 = (int)char4;
+    int int5 = (int)char5;
+    int int6 = (int)char6;
+    int int7 = (int)char7;
+    int int8 = (int)char8;
+    int int9 = (int)char9;
+    int total = int1+int2+int3+int4+int5+int6+int7+int8+int9;
+    
+    System.out.println(int1);
+    System.out.println(int2);
+    System.out.println(int3);
+    System.out.println(int4);
+    System.out.println(int5);
+    System.out.println(int6);
+    System.out.println(int7);
+    System.out.println(int8);
+    System.out.println(int9);
+    System.out.println(total);
+    }
+    
+}
